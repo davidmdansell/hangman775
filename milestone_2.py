8e2710cac2d87b382c56c80147dc46a561924998
David Ansell <davidmdansell@hotmail.com>
2023-11-29 17:14:50 +0000

    README updates and structure

--- milestone_2.py
+++ milestone_2.py
@@ -1,12 +1,14 @@
 import random
+#creates list of fruits, assigns to the variable "word_list"
 word_list = ['apple', 'grape', 'orange', 'watermelon', 'banana']
 print(word_list)
+#prints a random choice from the list and assigns to a variable "word"
 word = random.choice(word_list)
 print (word)
-
+#requests and validates user input to choose a letter, assigns to variable "choice"
 choice = input("Welcome to Hangman, please enter a letter:")
 print("You chose:" + choice)
-
+#if the input is a single letter, prints "good guess", otherwise tells the user that the input is not acceptable
 if len(choice) == 1 and choice.isalpha():
         print("Good guess")
 else:
