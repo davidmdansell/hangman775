94ddcd224462433e6662bbf0c7b14c8cbf33b004
David Ansell <davidmdansell@hotmail.com>
2023-11-30 10:33:42 +0000

    generates secret word from the list, requests and validates if the guess is a single letter

--- milestone_2.py
+++ milestone_2.py
@@ -6,7 +6,7 @@ def secret_word():
 #prints a random choice from the list and assigns to a variable "word"
     secret_word = random.choice(hangman_word_list)
     return(secret_word)
-    print (secret_word)
+print (secret_word)
 #requests and validates user input to choose a letter, assigns to variable "choice"
 def ask_for_input():
             choice = input("Welcome to Hangman, please enter a letter:")
@@ -15,7 +15,7 @@ def ask_for_input():
                       print("Thankyou")
             else: 
                     print("Oops! That is not a valid input. Please enter a single letter")
-
+            check_guess(choice)
 
 
        
