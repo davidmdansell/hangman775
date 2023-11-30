fe186979766923352c4fae147b1cdab24b6ce15f
David Ansell <davidmdansell@hotmail.com>
2023-11-30 10:25:39 +0000

    selects a secret word at random and asks for user to guess a letter

--- milestone_2.py
+++ milestone_2.py
@@ -1,16 +1,21 @@
 import random
-#creates list of fruits, assigns to the variable "word_list"
-word_list = ['apple', 'grape', 'orange', 'watermelon', 'banana']
-print(word_list)
+#creates list of fruits, assigns to the variable "hangman_word_list"
+def secret_word():
+    hangman_word_list = ['apple', 'grape', 'orange', 'watermelon', 'banana']
+    print(hangman_word_list)
 #prints a random choice from the list and assigns to a variable "word"
-word = random.choice(word_list)
-print (word)
+    secret_word = random.choice(hangman_word_list)
+    return(secret_word)
+    print (secret_word)
 #requests and validates user input to choose a letter, assigns to variable "choice"
-choice = input("Welcome to Hangman, please enter a letter:")
-print("You chose:" + choice)
-#if the input is a single letter, prints "good guess", otherwise tells the user that the input is not acceptable
-if len(choice) == 1 and choice.isalpha():
-        print("Good guess")
-else:
-        print("Oops! That is not a valid input. Please enter a single letter")
+def ask_for_input():
+            choice = input("Welcome to Hangman, please enter a letter:")
+            print("You chose:" + choice)
+            if len(choice) == 1 and choice.isalpha():
+                      print("Thankyou")
+            else: 
+                    print("Oops! That is not a valid input. Please enter a single letter")
+
+
+
        
