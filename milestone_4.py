8fcfbc56b74d0e5ba26b59525f2658cab0d9a066
David Ansell <davidmdansell@hotmail.com>
2023-12-02 14:20:33 +0000

    milestone 4 complete

--- milestone_4.py
+++ milestone_4.py
@@ -3,13 +3,14 @@ import random
 class Hangman:
     def __init__(self, word_list, num_lives=5):
 
-    #Initializes the Hangman game with the provided word_list and optional num_lives.
+        """Initializes the Hangman game with the provided word_list and optional num_lives"""
         self.word_list = word_list
         self.num_lives = num_lives
         self.word = random.choice(word_list).lower()
         self.word_guessed = ['_'] * len(self.word)
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
+        word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
 
     def display_word(self):
         return ' '.join(self.word_guessed)
@@ -18,10 +19,10 @@ class Hangman:
         return ', '.join(self.list_of_guesses)
 
     def ask_letter(self):
-    #Asks the user for a letter and checks two things:
-    # 1. If the letter has already been tried
-    # 2. If the character is a single character\
-    # If it passes both checks, it calls the check_letter method.
+        """Asks the user for a letter and checks two things:
+        1. If the letter has already been tried
+        2. If the character is a single character
+        If it passes both checks, it calls the check_letter method"""
     
         while True:
             # Ask the user for a letter
@@ -42,10 +43,8 @@ class Hangman:
             break
 
     def check_letter(self, letter):
-# Checks if the letter is in the word.
-# If it is, it replaces the '_' in the word_guessed list with the letter.
-# If it is not, it reduces the number of lives by 1.
-
+        """Checks if the letter is in the word. If it is, it replaces the '_' in the word_guessed list with the letter.
+        If it is not, it reduces the number of lives by 1."""
         letter = letter.lower()
         if letter in self.word:
             # Replace '_' in the word_guessed list with the letter for each occurrence
@@ -60,7 +59,9 @@ class Hangman:
         else:
             # Reduce the number of lives by 1
             self.num_lives -= 1
-            print("Incorrect guess. Lives remaining:", self.num_lives)
+            print(f"Sorry, {letter} is not in the word", self.num_lives)
+            print(f"You have {self.num_lives} left")
 
         print("Word:", self.display_word())
         print("Previous guesses:", self.display_guesses())
+
